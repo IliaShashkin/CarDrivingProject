65348e0d3abb8a83ad17d9856c0a0cbbcf7009a7
Comry <ComryOnWeb@foxotic.git.io>
2025-01-13 10:25:11 +0100

    fixed level 1 text and added small scrpit for smooth scroling

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿document.querySelectorAll('a.scroll-btn').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1); // Get the target ID
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth', // Smooth scrolling
+                block: 'start' // Align to the top of the section
+            });
+        }
+    });
+});
